3209ff10fed4e63640546c196e21a862206ba399
Nazarii Sheremet <xo8bit@gmail.com>
2017-04-06 11:09:57 +0300

    added sending files/docs/photos/audio

--- mrblib/sender.rb
+++ mrblib/sender.rb
@@ -22,24 +22,64 @@ module Sender
       disable_notification: dis_not
     }
 
-    if !keyboard.empty?
-      data[:reply_markup] = { keyboard: keyboard, resize_keyboard: resize, one_time_keyboard: one_time }
-    end
+    data = keyboard_add(data, keyboard)
+    send_file(api_doc_url, data, header_for_input(data[:document]))
+  end
 
-    if data[:document].kind_of?(File)
-      send_document(api_doc_url, data, {'Content-Type' => 'multipart/form-data'})
-    else
-      send_document(api_doc_url, data, {'Content-Type' => 'application/json'})
-    end
+  # Send Photo
+  def photo(msg, photo, caption = nil, keyboard = [], dis_not = false, resize = false, one_time = false)
+    data = {
+      chat_id: msg['chat']['id'],
+      photo: photo,
+      caption: caption,
+      disable_notification: dis_not
+    }
+
+    data = keyboard_add(data, keyboard)
+    send_file(api_photo_url, data, header_for_input(data[:photo]))
+  end
+
+  # Send Audio
+  def audio(msg, audio, caption = nil, keyboard = [], dis_not = false, resize = false, one_time = false)
+    data = {
+      chat_id: msg['chat']['id'],
+      audio: audio,
+      caption: caption,
+      disable_notification: dis_not
+    }
+
+    data = keyboard_add(data, keyboard)
+    send_file(api_photo_url, data, header_for_input(data[:audio]))
+  end
+
+  # Send Sticker
+  def sticker(msg, sticker, caption = nil, keyboard = [], dis_not = false, resize = false, one_time = false)
+    data = {
+      chat_id: msg['chat']['id'],
+      sticker: sticker,
+      disable_notification: dis_not
+    }
+
+    data = keyboard_add(data, keyboard)
+    send_file(api_photo_url, data, header_for_input(data[:audio]))
   end
 
 private
+
+  def keyboard_add(data, keyboard)
+    data[:reply_markup] = { keyboard: keyboard, resize_keyboard: resize, one_time_keyboard: one_time } unless keyboard.empty?
+    return data
+  end
+
+  def header_for_input(data)
+    data.kind_of?(File) ? {'Content-Type' => 'multipart/form-data'} : {'Content-Type' => 'application/json'}
+  end
 
   def send_message(url, data)
     HTTP.new(url).post(data, {'Content-Type' => 'application/json'})
   end
 
-  def send_document(url, data, headers = {})
+  def send_file(url, data, headers = {})
     HTTP.new(url).post(data, headers)
   end
 
@@ -49,5 +89,9 @@ private
 
   def api_doc_url
     base_url + "/sendDocument"
+  end
+
+  def api_photo_url
+    base_url + "/sendPhoto"
   end
 end
